58a82b21a7363b8af71234ba0916f23df7238438
Paul Chua <paul.chua96@gmail.com>
2025-06-18 18:33:06 -0400

    Removed extra functions and renamed main function

--- obsplugin.py
+++ obsplugin.py
@@ -1,39 +1,9 @@
 import cv2 as cv
 import numpy as np
-import time
 import os
-def take_screenshot():
-    
-    test_image = os.getcwd() + 'insert your path here'
-    img = cv.imread(test_image)
-    img2 = cv.imread(test_image,0)
-    
-    w,h = img2.shape[::-1]
-    your_side = img2[0:h, 0:w//2]
-    show_this = img[0:h, 0:w//2:]
-   
-    target_image = os.getcwd() + r'\photos\lose_image.png'
-    template = cv.imread(target_image,0)
-    cv.imshow("Template", template)
 
-    result = cv.matchTemplate(your_side,template, cv.TM_CCOEFF_NORMED)
-    _,max_val1, _, _ = cv.minMaxLoc(result)
-    threshold = .75
-    print(max_val1)
-    print(max_val1 >= threshold)
-    locations = np.where(result >= threshold)
-    print(locations)
-    for pt in zip(*locations[::-1]):
-        cv.rectangle(show_this,  pt, (pt[0] + w, pt[1] + h), (0, 255, 255), 2)
-        win_lose(0,0)
-    cv.imshow("Half",show_this)
-    cv.waitKey(0)
-    cv.destroyAllWindows()
-    
-    
-    
 
-def video_attempt():
+def read_video():
     file_path = 1
     capture = cv.VideoCapture(file_path)
     
@@ -104,4 +74,4 @@ def win_lose(w,l):
     file.close()
     
 if __name__ == "__main__":
-    video_attempt()
+    read_video()
